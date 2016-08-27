730688d3adef2f8316a2fa1e8fabea476151b31b
Chris Custine <ccustine@apache.org>
2016-08-27 17:35:46 -0600

    Use protobuf format for receiving messages Add test route for local debugging

--- com.redhat.iot.vertica.demo.camel/src/main/java/com/redhat/iot/vertica/demo/camel/GatewayRouter.java
+++ com.redhat.iot.vertica.demo.camel/src/main/java/com/redhat/iot/vertica/demo/camel/GatewayRouter.java
@@ -1,7 +1,14 @@
 package com.redhat.iot.vertica.demo.camel;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.dataformat.csv.CsvDataFormat;
 import org.eclipse.kura.camel.cloud.KuraCloudComponent;
 import org.eclipse.kura.camel.router.CamelRouter;
+import org.eclipse.kura.message.KuraPayload;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * HPE Vertica demo route.
@@ -9,27 +16,47 @@ import org.eclipse.kura.camel.router.CamelRouter;
 public class GatewayRouter extends CamelRouter {
 
    private static String KURA = "kura-cloud:";
-   private static String TOPIC = "vertica-demo/notifications";
+   private static String TOPIC = "vertica-demo/data";
 
    @Override
     public void configure() throws Exception {
         KuraCloudComponent cloudComponent = new KuraCloudComponent();
         cloudComponent.setCamelContext(camelContext);
         camelContext.addComponent("kura-cloud", cloudComponent);
-//        WebsocketComponent websocketComponent = new WebsocketComponent();
-//        camelContext.addComponent("atmosphere-websocket", websocketComponent);
 
-//      Generate fake data for testing every 5 seconds, send to MQTT topic
-//      Comment out for real demo
-      from("timer://heartbeat?fixedRate=true&period=5000")
-          .setBody(simple("Hello from timer at ${header.firedTime}"))
-          .to(KURA + TOPIC);
+
+      from("file:/media/sf_data/?fileName=Vertica_Data_PM.csv&delete=true") //Poll for file and delete when finished
+            .split().tokenize("\\n").streaming() //Process each line of the file separately, and stream to keep memory usage down
+            .delay(1000) //Delay 1 second between processing lines
+            .log("Sending ${header.CamelSplitIndex} of ${header.CamelSplitSize}")
+            .unmarshal(new CsvDataFormat() //Unmarshal from the csv formatted line and convert to a map using the specified strings as keys
+                  .setIgnoreEmptyLines(true)
+                  .setUseMaps(true)
+                  .setCommentMarker('#')
+                  .setHeader(new String[]{"id", "cycle", "settings1", "settings2", "settings3", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "s12", "s13", "s14", "s15", "s16", "s17", "s18", "s19", "s20", "s21"}))
+            .process(new Processor() {
+               @Override
+               public void process(Exchange exchange) throws Exception {
+                  KuraPayload payload = new KuraPayload();
+                  List<Map> metrics = (List<Map>) exchange.getIn().getBody();
+                  Map<String, String> map =  metrics.get(0); //Each line of the file produces a map of name/value pairs, but we only get one line at a time due to the splitter above
+                  for (Map.Entry<String, String> entry : map.entrySet()) {
+                     payload.addMetric(entry.getKey(), entry.getValue());
+                  }
+
+                  exchange.getIn().setBody(payload);
+               }
+            })
+            .log("Sending CSV record")
+            .to(KURA + TOPIC);
+
 
       // Subscribe to topics on Everyware Cloud
+/*
       from(KURA + TOPIC)
          .log("Received Test Message: ${body.body}")
-//         .to("atmosphere-websocket:///notification");
          .to("log:TestReceived");
+*/
 
 
    }
---
+++ com.redhat.iot.vertica.demo.camel/src/main/java/com/redhat/iot/vertica/demo/camel/TestRoute.java
@@ -0,0 +1,52 @@
+package com.redhat.iot.vertica.demo.camel;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.eclipse.kura.core.message.protobuf.KuraPayloadProto;
+
+/**
+ * HPE Vertica demo route.
+ */
+public class TestRoute extends RouteBuilder {
+
+   @Override
+    public void configure() throws Exception {
+
+//      Generate fake data for testing every 5 seconds, send to MQTT topic
+//      Comment out for real demo
+//      from("timer://heartbeat?fixedRate=true&period=5000")
+
+      /*
+      from("file:/Users/ccustine/development/redhat/rh-vertica-demo/com.redhat.iot.vertica.demo.camel/src/test/resources/?fileName=Vertica_Data_PM_short.csv&noop=true")
+            .split().tokenize("\\n").streaming()
+            .delay(1000)
+            .log("Sending ${header.CamelSplitIndex} of ${header.CamelSplitSize}")
+            .unmarshal(new CsvDataFormat()
+                  .setIgnoreEmptyLines(true)
+                  .setUseMaps(true)
+                  .setCommentMarker('#')
+                  .setHeader(new String[]{"id", "cycle", "settings1", "settings2", "settings3", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "s12", "s13", "s14", "s15", "s16", "s17", "s18", "s19", "s20", "s21"}))
+            .to("log:Send");
+*/
+
+      // Subscribe to topics, unmarshal Kura gzipped/protobuf messages
+      from("mqtt:consume?host=tcp://172.16.121.81:1883&userName=admin&password=admin&subscribeTopicName=Red-Hat/#/vertica-demo/data")
+         .unmarshal().gzip()
+         .unmarshal().protobuf("org.eclipse.kura.core.message.protobuf.KuraPayloadProto$KuraPayload")
+            .process(new Processor() {
+               @Override
+               public void process(Exchange exchange) throws Exception {
+                  KuraPayloadProto.KuraPayload kuraPayload = (KuraPayloadProto.KuraPayload) exchange.getIn().getBody();
+                  for (KuraPayloadProto.KuraPayload.KuraMetric kuraMetric : kuraPayload.getMetricList()) {
+                     String name = kuraMetric.getName();
+                     String value = kuraMetric.getStringValue();
+                     // Do something with the metric name/value here and then send it on
+                  }
+               }
+            })
+         .to("mock:Received");
+
+
+   }
+}
